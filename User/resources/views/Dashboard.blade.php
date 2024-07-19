d4148112845cbbf0efdd80e9ea2268bc0bf0c8a7
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-07-19 09:42:47 +0300

    profil et Dashboard avec page Home

--- User/resources/views/Dashboard.blade.php
+++ User/resources/views/Dashboard.blade.php
@@ -1,3 +1,238 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Ministere de la Justice</title>
+
+    <link href="{{ asset('assets/images/icon/minjus.jpg') }}" rel="icon" media="all">
+
+    <!-- Fontfaces CSS-->
+    <link href="{{ asset('assets/css/font-face.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="{{ asset('assets/vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="{{ asset('assets/vendor/animsition/animsition.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/wow/animate.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/slick/slick.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+    <!-- Main CSS-->
+    <link href="{{ asset('assets/css/theme.css') }}" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+<div class="page-wrapper">
+    <!-- HEADER MOBILE-->
+    <header class="header-mobile d-block d-lg-none">
+        <div class="header-mobile__bar">
+            <div class="container-fluid">
+                <div class="header-mobile-inner">
+                    <a class="logo" href="/">
+                        <img src="{{ asset('assets/images/icon/logo.png') }}" alt="CoolAdmin" />
+                    </a>
+                    <button class="hamburger hamburger--slider" type="button">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar-mobile">
+            <div class="container-fluid">
+                <ul class="navbar-mobile__list list-unstyled">
+
+                    <li>
+                        <a href="/">
+                            <i class="fas fa-chart-bar"></i>Tableau de bord</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-user"></i>Utilisateurs</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-leaf"></i>Camp penal</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-map-marker-alt"></i>Carte</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- END HEADER MOBILE-->
+
+    <!-- MENU SIDEBAR-->
+    <aside class="menu-sidebar d-none d-lg-block">
+        <div class="logo">
+            <a href="/">
+                <img src="{{ asset('assets/images/icon/logo.png') }}" alt="Minister de la justice" />
+            </a>
+        </div>
+        <div class="menu-sidebar__content js-scrollbar1">
+            <nav class="navbar-sidebar">
+                <ul class="list-unstyled navbar__list">
+                    {{--                    <li class="active has-sub">--}}
+                    {{--                        <a class="js-arrow" href="#">--}}
+                    {{--                            <i class="fas fa-tachometer-alt"></i>Dashboard</a>--}}
+                    {{--                        <ul class="list-unstyled navbar__sub-list js-sub-list">--}}
+                    {{--                            <li>--}}
+                    {{--                                <a href="index.html">Dashboard 1</a>--}}
+                    {{--                            </li>--}}
+                    {{--                            <li>--}}
+                    {{--                                <a href="index2.html">Dashboard 2</a>--}}
+                    {{--                            </li>--}}
+                    {{--                            <li>--}}
+                    {{--                                <a href="index3.html">Dashboard 3</a>--}}
+                    {{--                            </li>--}}
+                    {{--                            <li>--}}
+                    {{--                                <a href="index4.html">Dashboard 4</a>--}}
+                    {{--                            </li>--}}
+                    {{--                        </ul>--}}
+                    {{--                    </li>--}}
+                    <li>
+                        <a href="/">
+                            <i class="fas fa-chart-bar"></i>Tableau de bord</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-user"></i>Utilisateurs</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-leaf"></i>Camp penal</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-map-marker-alt"></i>Carte</a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </aside>
+    <!-- END MENU SIDEBAR-->
+
+    <!-- PAGE CONTAINER-->
+    <div class="page-container">
+        <!-- HEADER DESKTOP-->
+        <header class="header-desktop">
+            <div class="section__content section__content--p30">
+                <div class="container-fluid">
+                    <div class="header-wrap">
+                        <div class="col-lg-10"></div>
+                        <div class="account-wrap col-lg-2">
+                            <div class="account-item clearfix js-item-menu">
+                                <div class="image">
+                                    <img src="{{ asset('assets/images/icon/avatar-01.jpg') }}" alt="John Doe" />
+                                </div>
+                                <div class="content">
+                                    <a class="js-acc-btn" href="#">{{ Auth::user()->name }}</a>
+                                </div>
+                                <div class="account-dropdown js-dropdown">
+                                    <div class="info clearfix">
+                                        <div class="image">
+                                            <a href="#">
+                                                <img src="{{ asset('assets/images/icon/avatar-01.jpg') }}" alt="John Doe" />
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <h5 class="name">
+                                                <a href="#">{{ Auth::user()->name }}</a>
+                                            </h5>
+                                            <span class="email">{{ Auth::user()->email }}</span>
+                                        </div>
+                                    </div>
+                                    <div class="account-dropdown__body">
+                                        <div class="account-dropdown__item">
+                                            <a href="{{ route('profile.edit') }}">
+                                                <i class="zmdi zmdi-account"></i>Account</a>
+                                        </div>
+                                    </div>
+
+
+                                    </form>
+                                    <div class="account-dropdown__footer">
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <x-dropdown-link :href="route('logout')"
+                                                             onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                                <i class="zmdi zmdi-power"></i>
+                                                {{ __('Logout') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- HEADER DESKTOP-->
+
+        <!-- MAIN CONTENT-->
+        <div class="main-content">
+            <div class="section__content section__content--p30">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+        <!-- END MAIN CONTENT-->
+        <!-- END PAGE CONTAINER-->
+    </div>
+
 </div>
+
+<!-- Jquery JS-->
+<script src="{{ asset('assets/vendor/jquery-3.2.1.min.js') }}"></script>
+<!-- Bootstrap JS-->
+<script src="{{ asset('assets/vendor/bootstrap-4.1/popper.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap-4.1/bootstrap.min.js') }}"></script>
+<!-- Vendor JS       -->
+<script src="{{ asset('assets/vendor/slick/slick.min.js') }}">
+</script>
+<script src="{{ asset('assets/vendor/wow/wow.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/animsition/animsition.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js') }}">
+</script>
+<script src="{{ asset('assets/vendor/counter-up/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/counter-up/jquery.counterup.min.js') }}">
+</script>
+<script src="{{ asset('assets/vendor/circle-progress/circle-progress.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('assets/vendor/chartjs/Chart.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/select2/select2.min.js') }}">
+</script>
+<!-- Main JS-->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+<script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+
+
+</body>
+
+</html>
+<!-- end document-->
