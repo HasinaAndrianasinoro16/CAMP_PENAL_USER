80e5664507d59f4b78ceaf861874453f9e8d3404
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-07-24 14:07:04 +0300

    Materiel model et controller

--- User/resources/views/Materiel.blade.php
+++ User/resources/views/Materiel.blade.php
@@ -1,3 +1,35 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+@extends('Dashboard')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="overview-wrap">
+                <h2 class="title-1">Materiel</h2>
+            </div>
+            <div class="py-3"></div>
+        </div>
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title h3">Formulaire d'ajout de collaborateur</div>
+                    <form action="{{ route('SaveCollaborateur') }}" method="post">
+                        @csrf
+                        <div class="card-body card-block">
+                            <div class="form-group">
+                                <div class="col-8">
+                                    <label for="Nom" class="form-control-label">Nom</label>
+                                    <input type="text" id="Nom" name="nom" placeholder="Entrer le nom" class="form-control">
+                                </div>
+                            </div>
+                            <div class="py-2"></div>
+                            <div class="form-group">
+                                <div class="col-11">
+                                    <button type="submit" class="btn btn-success btn-lg"><i class="fas fa-check"></i> Enregistrer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
